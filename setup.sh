88043510e8ba8ae8424c559b36700e5a18792ae5
Santiago Narvaez <santiagonar1@gmail.com>
2025-02-13 16:23:33 +0100

    feat: add gitconfig

--- setup.sh
+++ setup.sh
@@ -5,4 +5,6 @@ ln -s $(pwd)/bash/bashrc ~/.bashrc
 ln -s $(pwd)/vim/vimrc ~/.vimrc
 ln -s $(pwd)/zsh/.zshrc ~/.zshrc
 ln -s $(pwd)/starship/starship.toml ~/.config/starship.toml
+ln -s $(pwd)/git/gitconfig ~/.gitconfig
+ln -s $(pwd)/git/gitconfig-tum ~/.gitconfig-tum
 
